e0723d98b6dc441d564d0c04422e46ede850fe96
gianlucanied <gianluca.nieddu96@gmail.com>
2023-11-20 16:45:31 +0100

    stampa in console funzionante ma non in innerhtml

--- script.js
+++ script.js
@@ -10,19 +10,16 @@ L’output del prezzo finale va messo fuori in forma umana (con massimo due deci
 
 const inputNome = document.querySelector(".nomeuser");
 
-const sendButton = document.getElementById("send");
-
 let nomeUser;
 
 console.log(inputNome);
 
 console.log(inputNome.value);
 
+
 // Chiediamo a User quanti km dovrà percorrere
 
 const inputKm = document.querySelector(".kmuser");
-
-const sendButton = document.getElementById("send");
 
 let kmUser;
 
@@ -30,16 +27,19 @@ console.log(inputKm);
 
 console.log(inputKm.value);
 
-
 // Chiediamo a User la sua età
 
+const inputEta = document.querySelector(".etauser");
 
+let etaUser;
 
+console.log(inputEta);
 
+console.log(inputEta.value);
 
 // Selezione button per inviare
 
-
+const sendButton = document.getElementById("send");
 
 sendButton.addEventListener ("click",
 
@@ -49,11 +49,14 @@ sendButton.addEventListener ("click",
 
         let kmUser = inputKm.value;
         console.log(kmUser)
+
+        let etaUser = inputEta.value;
+        console.log(etaUser);
     }
 
 )
 
-console.log(userKm, userAge);
+console.log(kmUser, etaUser);
 
 // Definiamo i 0.21 € al km
 
@@ -61,20 +64,19 @@ const prezzoOgniKm = 0.21;
 
 // Indichiamo come si calcola il prezzo, la frase pre costo e l'euro post costo
 
-const BigliettoNoSconto = userKm * prezzoOgniKm;
+const BigliettoNoSconto = kmUser * prezzoOgniKm;
 
-const frase = "Il prezzo del tuo biglietto è ";
 const euro = " €";
 
 // Applichiamo sconto del 20% per i minorenni
 
-if (userAge <= 18) {
+if (etaUser <= 18) {
     document.getElementById("mio_id").innerHTML = ((BigliettoNoSconto / 100) *80).toFixed(2) + euro;
 }
 
 // Applichiamo sconto del 40% per over 65
 
-else if (userAge >= 65) {
+else if (etaUser >= 65) {
     document.getElementById("mio_id").innerHTML = ((BigliettoNoSconto / 100) *60).toFixed(2) + euro;
 }
 
